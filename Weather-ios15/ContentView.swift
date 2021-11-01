9118f153e89304c00af296ec07fcd412214fe055
chrisais9 <chrisais9@naver.com>
2021-11-01 14:59:04 +0900

    Initial Commit

---
+++ Weather-ios15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather-ios15
+//
+//  Created by chris on 2021/11/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather-ios15/Weather_ios15App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ios15App.swift
+//  Weather-ios15
+//
+//  Created by chris on 2021/11/01.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ios15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
